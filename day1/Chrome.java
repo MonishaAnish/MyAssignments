886ad020904338098ee880fc7c6f007b8fb31418
MonishaAnish <80351362+MonishaAnish@users.noreply.github.com>
2024-12-09 19:39:35 +0530

    Add files via upload
    Week1.Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float version=91.0f;
+String developer="Google";
+boolean isBeta=false;
+int releaseYear=2008;
+char shortCutKey='C';
+System.out.println("Version of Chrome is " +version);
+System.out.println("developer of String is "+developer);
+System.out.println("isBeta of boolean is "+isBeta);
+System.out.println("releseYear of int is "+releaseYear);
+System.out.println("shortCutKey of char is "+shortCutKey);
+
+
+	}
+
+}
